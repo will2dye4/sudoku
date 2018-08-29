707741ccb97a859bc1e83c5b6a156e92efd97367
wdye <wdye@pindrop.com>
2018-08-29 17:09:40 -0400

    More performance tests

--- scripts/performance_test.py
+++ scripts/performance_test.py
@@ -1,41 +1,70 @@
+#!/usr/bin/env python3.7
+
 import time
 
 from collections import namedtuple
-from typing import Iterable
+from typing import (
+    AnyStr,
+    Iterable,
+    List,
+)
 
 from sudoku import (
     SolutionAlgorithm,
-    get_puzzle_by_name,
+    Sudoku,
+)
+from sudoku.sample_puzzles import (
+    empty_cell_count_puzzles,
+    hard_puzzles,
 )
 
 
 Trial = namedtuple('Trial', ['name', 'empty_cells', 'possibilities_tried', 'backtracks', 'total_time'])
 
 
-def test_time_vs_number_of_empty_cells(algorithm: SolutionAlgorithm) -> Iterable[Trial]:
+def test_brute_force_solve_time() -> Iterable[Trial]:
+    return test_solve_time(hard_puzzles, SolutionAlgorithm.BRUTE_FORCE)
+
+
+def test_constraint_based_solve_time() -> Iterable[Trial]:
+    return test_solve_time(hard_puzzles, SolutionAlgorithm.CONSTRAINT_BASED)
+
+
+def test_dlx_solve_time() -> Iterable[Trial]:
+    return test_solve_time(hard_puzzles, SolutionAlgorithm.DANCING_LINKS)
+
+
+def test_empty_cells_vs_solve_time(algorithm: SolutionAlgorithm) -> Iterable[Trial]:
+    return test_solve_time(empty_cell_count_puzzles, algorithm, 'empty')
+
+
+def test_solve_time(puzzle_strings: List[AnyStr], algorithm: SolutionAlgorithm, trial_tag: AnyStr = '') -> Iterable[Trial]:
     results = []
 
-    for i in range(1, 65):
-        puzzle_string = get_puzzle_by_name(f'empty-{i}')
+    for i, puzzle_string in enumerate(puzzle_strings):
         sudoku_type, solver_type = algorithm.value
         sudoku = sudoku_type.from_string(puzzle_string)
+        empty_cells = Sudoku.GRID_SIZE - len(sudoku.clue_cells)
         solver = solver_type(sudoku)
 
-        print(f'Running {algorithm.name} algorithm with {i} empty cells')
+        print(f'Running {algorithm.name} algorithm trial #{i+1} of {len(puzzle_strings)}')
         start = time.time()
         solved = solver.solve()
         end = time.time()
 
         if not solved:
-            raise RuntimeError(f'Failed to solve puzzle with {i} empty cells using {algorithm.name} algorithm')
+            raise RuntimeError(f'Failed to solve trial puzzle #{i+1} using the {algorithm.name} algorithm')
 
         alg_name = 'brute' if algorithm == SolutionAlgorithm.BRUTE_FORCE else \
             'constraint' if algorithm == SolutionAlgorithm.CONSTRAINT_BASED else 'dlx'
-        results.append(Trial(f'{alg_name}-{i}-empty', i, solver.possibilities_tried, solver.backtracks, end - start))
+        trial_name = f'{alg_name}-{i+1}'
+        if trial_tag:
+            trial_name += f'-{trial_tag}'
+        results.append(Trial(trial_name, empty_cells, solver.possibilities_tried, solver.backtracks, end - start))
 
     return results
 
 
 if __name__ == '__main__':
     for algorithm in SolutionAlgorithm:
-        test_time_vs_number_of_empty_cells(algorithm)
+        test_empty_cells_vs_solve_time(algorithm)
